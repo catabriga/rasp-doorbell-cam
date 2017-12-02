35ad322d7695c8b11d4a33d84ae839673980e3b9
Lucas Catabriga <catabriga90@gmail.com>
2017-12-01 22:46:49 -0200

    Raspicam hello world

--- main.cpp
+++ main.cpp
@@ -5,28 +5,60 @@
 #include <fstream>
 #include <iostream>
 #include <raspicam/raspicam.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 using namespace std;
  
-int main ( int argc,char **argv ) {
+int main ( int argc,char **argv ) 
+{
 	raspicam::RaspiCam Camera; //Camera object
+
 	//Open camera 
 	cout<<"Opening Camera..."<<endl;
-	if ( !Camera.open()) {cerr<<"Error opening camera"<<endl;return -1;}
+	if ( !Camera.open()) 
+    {
+        cerr<<"Error opening camera"<<endl;return -1;
+    }
+
 	//wait a while until camera stabilizes
 	cout<<"Sleeping for 3 secs"<<endl;
 	usleep(3000000);
-	//capture
-	Camera.grab();
+
 	//allocate memory
-	unsigned char *data=new unsigned char[  Camera.getImageTypeSize ( raspicam::RASPICAM_FORMAT_RGB )];
-	//extract the image in rgb format
-	Camera.retrieve ( data,raspicam::RASPICAM_FORMAT_RGB );//get camera image
-	//save
-	std::ofstream outFile ( "raspicam_image.ppm",std::ios::binary );
-	outFile<<"P6\n"<<Camera.getWidth() <<" "<<Camera.getHeight() <<" 255\n";
-	outFile.write ( ( char* ) data, Camera.getImageTypeSize ( raspicam::RASPICAM_FORMAT_RGB ) );
-	cout<<"Image saved at raspicam_image.ppm"<<endl;
+    int imgSize = Camera.getImageTypeSize(raspicam::RASPICAM_FORMAT_RGB)
+	unsigned char *data=new unsigned char[imgSize];
+
+    int imgCount = 0;
+    while(1)
+    {
+	    //capture
+	    Camera.grab();
+
+	    //extract the image in rgb format
+	    Camera.retrieve ( data,raspicam::RASPICAM_FORMAT_RGB );//get camera image
+
+        long imgBrightness = 0;
+        for(int i=0; i<imgSize; i++)
+        {
+            imgBrightness += data[i];
+        }
+
+        cout << "Brightness: " << imgBrightness << endl;
+
+        if(imgBrightness > 4000)
+        {
+	        //save
+	        std::ofstream outFile("img"+imgCount+".ppm",std::ios::binary);
+	        outFile << "P6\n" << Camera.getWidth() << " " << Camera.getHeight() << " 255\n";
+	        outFile.write( ( char* ) data, Camera.getImageTypeSize ( raspicam::RASPICAM_FORMAT_RGB ) );
+            imgCount++;
+	        //cout<<"Image saved at raspicam_image.ppm"<<endl;
+        }
+    }
+
 	//free resrources    
 	delete data;
+
 	return 0;
 }
